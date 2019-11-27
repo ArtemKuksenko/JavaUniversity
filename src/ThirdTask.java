796306efc2b3d0831cd73e391dfb97d229276de5
Kuksenko Artem <kuksenko.artem@gmail.com>
2019-11-27 21:09:54 +0300

    files

--- src/ThirdTask.java
+++ src/ThirdTask.java
@@ -1,11 +1,20 @@
 import java.io.File;
+import java.io.IOException;
 
 public class ThirdTask {
 
     public static void main(String[] args) {
         File[] arrFiles = new File[10];
+        File dir = new File("/home/co/IdeaProjects/1stTask/src/files");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            arrFiles[i] = new File(dir,String.valueOf(i) + ".txt" );
+            try {
+                arrFiles[i].createNewFile();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
     }
 
